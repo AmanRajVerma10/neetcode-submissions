97b1b25b4e1cc642ed0bee2ef5fd20aac9d086ae
AmanRajVerma10 <144332100+AmanRajVerma10@users.noreply.github.com>
2026-08-26 16:10:08 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l=0,r=1;
+        int profit=0;
+        while(r<prices.size()){
+            profit=max(profit,prices[r]-prices[l]);
+            if(prices[r]<prices[l])l=r;
+            r++;
+        }
+        return profit;
+    }
+};
